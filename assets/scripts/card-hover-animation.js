156dbe660538e59a8ce871249bb80005c0b235cc
Dev. Pablo H. <146978584+devpabloh@users.noreply.github.com>
2024-05-16 09:32:15 -0300

    atualização

---
+++ assets/scripts/card-hover-animation.js
@@ -0,0 +1,14 @@
+function addEventListernersToCards(){
+    const cardElements = document.getElementsByClassName('s-card');
+    console.log(cardElements)
+}
+
+/* 
+Quando colocamos a tag script dentro do head do HTML, usamos esse evento para informar que primeiro deve ser ligo/carregado os elementos HTML, para depois realizar a leitura/carregamento a parte do script.
+
+
+document.addEventListener("DOMContentLoaded", addEventListernersToCards, false);
+
+*/
+
+

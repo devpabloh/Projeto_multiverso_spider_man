4cca202389acb9e335103b1aa3d7bd65cb139c7d
Dev. Pablo H. <146978584+devpabloh@users.noreply.github.com>
2024-05-17 07:09:41 -0300

    atualização

--- assets/scripts/card-hover-animation.js
+++ assets/scripts/card-hover-animation.js
@@ -2,12 +2,17 @@ function handleMouseEnter(){
     this.classList.add('s-card--hovered');
 }
 
+function handMouseLeave(){
+    this.classList.remove('s-card--hover')
+}
+
 function addEventListernersToCards(){
     const cardElements = document.getElementsByClassName('s-card');
 
     for(let i = 0; i < cardElements.length; i++){
         const card = cardElements[i];
         card.addEventListener('mouseenter', handleMouseEnter);
+        card.addEventListener('mouseleave',handMouseLeave)
 
     }
 }
